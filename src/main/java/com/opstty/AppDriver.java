111484911a65ac83d748a8341e4d097bfc22b963
ViLonArt <vincent.tran.12139@gmail.com>
2021-10-11 21:24:17 +0200

    add jobs in program driver

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,6 @@
 package com.opstty;
 
-import com.opstty.job.WordCount;
+import com.opstty.job.*;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,7 +11,18 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("districtwithtrees", DistrictwithTrees.class,
+                    "A map/reduce program that returns the distinct districts with trees");
+            programDriver.addClass("heightsortedtrees", HeightSortedTrees.class,
+                    "A map/reduce program that sort trees by height");
+            programDriver.addClass("oldesttree", OldestTree.class,
+                    "A map/reduce program that returns the oldest tree of all districts");
+            programDriver.addClass("treesbykind", TreesbyKind.class,
+                    "A map/reduce program that returns the trees sorted by kind");
+            programDriver.addClass("treesmaxheight", TreesMaxHeight.class,
+                    "A map/reduce program that returns the maximum height per kind of tree");
+            programDriver.addClass("treespecies", TreeSpecies.class,
+                    "A map/reduce program that returns the different species of trees");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
